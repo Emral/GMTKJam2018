c04c5d6ea3a507760b3d0070a517bb2df1bf4774
Lena <42876494+ChronicHeart@users.noreply.github.com>
2018-08-31 19:27:48 -0500

    Pinball Script
    I wrote a basic script that will prevent the ball from surpassing a specified vertical speed. I attatched this to Enjl's ball prefab while I was at it

---
+++ Assets/Scripts/PinballScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------------
+   Author: Lena
+   Purpose: To prevent the ball from going beyond its maximum speed in the vertical direction. 
+  ------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+public class PinballScript : MonoBehaviour {
+
+    public float maxVelocity = 2f;       // This is the maximum speed the ball can fall
+
+    Rigidbody2D rig;                    // Reference to the rigidbody
+
+    private void Start()
+    {
+        //Set references
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        //If the ball is going to fast upwards, set it to its maximum speed
+        if (rig.velocity.y > maxVelocity)
+        {
+            rig.velocity = new Vector2(rig.velocity.x, maxVelocity);
+            //Debug.Log(rig.velocity.y);
+        }
+
+        //If the ball is going to fast downwards, set it to its maximum speed
+        if (rig.velocity.y < -maxVelocity)
+        {
+            rig.velocity = new Vector2(rig.velocity.x, -maxVelocity);
+            //Debug.Log(rig.velocity.y);
+        }        
+    }
+}
