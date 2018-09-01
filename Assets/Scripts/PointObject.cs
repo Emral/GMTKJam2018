eb02a50fc0c2a12786756717150b9a9c86a98e63
Lena <42876494+ChronicHeart@users.noreply.github.com>
2018-09-01 13:47:06 -0500

    PointObject Script
    A simple script that scores x points when the ball collides with it. It was harder than it needed to be because I'm a dork.

---
+++ Assets/Scripts/PointObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointObject : MonoBehaviour {
+
+    public int pointValue = 500;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GameObject.Find("UI").GetComponent<GameManager>().score += pointValue;
+        }
+    }
+
+}
