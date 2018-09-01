322bcd4569399d71a14d8693767291c1fc5fbd04
Lena <42876494+ChronicHeart@users.noreply.github.com>
2018-09-01 18:33:23 -0500

    Camera Controller Updated
    The camera script used to work with Vector2, but this ended up putting it on the 0 in the z-axis, the same position of the game. I switched it to Vector3 so we wouldn't have to worry about that anymore.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     public float smoothing = .025f;     // Controls how quickly the camera moves
-    public Vector2 targetPosition;      // Where the camera is trying to be
+    public Vector3 targetPosition;      // Where the camera is trying to be
     public float targetSize;            // The size the camera is trying to maintain (This assumes the camera is orthagraphic)
     private Camera cam;                 // Get a reference to the camera to avoid GetComponent twice per frame...
 
@@ -20,7 +20,7 @@ public class CameraController : MonoBehaviour {
     // Update size and location according to the target variables. Target variables are changed by outside scripts
 	void Update ()
     {
-        transform.position = Vector2.Lerp(transform.position, targetPosition, smoothing);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
         cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetSize, smoothing);
     }
 }
