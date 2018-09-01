757b83ef304ad8cf619a78455a9e79cf917bf68d
Lena <42876494+ChronicHeart@users.noreply.github.com>
2018-08-31 21:53:38 -0500

    Camera Controller and Camera Transitons
    I added two scripts. One is the Camera Controlller, which makes sure the camera moves to wherever it needs to be. The other is the Camera Transition script; when the ball hits a trigger with this script, it will update the camera's settings.
    
    Note that the camera has to be on 0 in the z-axis and in orthagraphic mode to work. If we need a perspective camera, I can change it.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float smoothing = .025f;     // Controls how quickly the camera moves
+    public Vector2 targetPosition;      // Where the camera is trying to be
+    public float targetSize;            // The size the camera is trying to maintain (This assumes the camera is orthagraphic)
+
+	void Start ()
+    {
+        // Set the targets to the camera's inital position
+        targetPosition = transform.position;
+        targetSize = GetComponent<Camera>().orthographicSize;
+	}
+	
+    // Update size and location according to the target variables. Target variables are changed by outside scripts
+	void Update ()
+    {
+        transform.position = Vector2.Lerp(transform.position, targetPosition, smoothing);
+        GetComponent<Camera>().orthographicSize = Mathf.Lerp(GetComponent<Camera>().orthographicSize, targetSize, smoothing);
+    }
+}
---
+++ Assets/Scripts/CameraTransition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------
+   Author: Lena
+   Purpose: To move the camera to a new position when a player enters a new section of the board. The size of the camera can also be adjusted
+-------------------------------------------------------------------------------------------------------------------------------------------*/
+
+public class CameraTransition : MonoBehaviour
+{
+
+    public Vector3 newPosition;     // Position the camera will move to
+    public int newSize;             // The new orthographic size of the camera
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Camera.main.GetComponent<CameraController>().targetPosition = newPosition;
+            Camera.main.GetComponent<CameraController>().targetSize = newSize;
+        }
+    }
+}
