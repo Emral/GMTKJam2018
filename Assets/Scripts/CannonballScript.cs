7189fb47834c22f38eba10bd47a9e4f8ff7d4a9f
Lena <42876494+ChronicHeart@users.noreply.github.com>
2018-09-01 21:32:22 -0500

    Cannonball Active; not Cannon
    Created the script for the cannonball itself. The cannon itself is not ready in any sense of the word. Created a prefab for the test cannonball

---
+++ Assets/Scripts/CannonballScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonballScript : MonoBehaviour {
+
+    public float speed = 5f;        // Speed at which the ball travels
+    public float radius = 5f;       // The radius in which the cannonball affects the player
+    public float force = 15f;       // The strength of the cannonball's explosion upon destruction
+
+    Rigidbody2D rig;                // Reference to the rigidbody
+
+	// Use this for initialization
+	void Start () {
+        rig = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+    {
+        //Move the ball towards the right
+        rig.velocity = rig.transform.right * speed;
+	}
+
+    //This is more or less copied and pasted from bombscript. Only difference; object destroyes itself instead of deactivating.
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Transform playerTransform = GameManager.instance.player.transform;
+        Vector2 dist = playerTransform.position - transform.position;
+        if (dist.magnitude < radius && dist.sqrMagnitude > 0)
+        {
+            playerTransform.GetComponent<Rigidbody2D>().AddForce(dist.normalized * (force / dist.sqrMagnitude), ForceMode2D.Impulse);
+        }
+        Destroy(gameObject);
+    }
+}
