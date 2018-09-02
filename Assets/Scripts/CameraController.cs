ee32d95c805d14d01ab129d3126571c9f7e250e3
Enjl <enjlectric@gmail.com>
2018-09-02 18:31:52 +0200

    safety

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-    public float smoothing = .025f;     // Controls how quickly the camera moves
+    public float smoothing = 1;         // Controls how quickly the camera moves
     public Vector3 targetPosition;      // Where the camera is trying to be
     public float targetSize;            // The size the camera is trying to maintain (This assumes the camera is orthagraphic)
     private Camera cam;                 // Get a reference to the camera to avoid GetComponent twice per frame...
@@ -20,7 +20,15 @@ public class CameraController : MonoBehaviour {
     // Update size and location according to the target variables. Target variables are changed by outside scripts
 	void Update ()
     {
-        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
-        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetSize, smoothing);
+        if (transform.position != targetPosition && smoothing >= 1){
+            Time.timeScale = 0;
+            smoothing = 0;
+        }
+        if (!PauseMenu.paused && smoothing < 1){
+            smoothing = Mathf.Min(smoothing + 0.02f, 1);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetSize, smoothing);
+            Time.timeScale = smoothing;
+        }
     }
 }
