3f64e1a1d771fa6c9183a86a254e44a81eded2a2
Enjl <enjlectric@gmail.com>
2018-09-02 02:06:46 +0200

    draggable object and cursors although draggable object is sorta jank

---
+++ Assets/Scripts/CursorHelper.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorHelper : MonoBehaviour {
+    public Texture2D mainTex;
+    public Texture2D grabTex;
+
+    private Vector3 lastMousePos = Vector3.zero;
+	// Use this for initialization
+	void Start () {
+        SetDefault();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPos.z = -10;
+        RaycastHit2D[] hitResults = new RaycastHit2D[5];
+        ContactFilter2D filter = new ContactFilter2D();
+        int hitRay = Physics2D.Raycast(worldPos, Vector3.forward, filter, hitResults, 100);
+        if (hitRay > 0){
+            foreach(RaycastHit2D results in hitResults) {
+                if (results.transform == null) continue;
+                if (results.transform.gameObject.layer == 9) {
+                    SetGrab();
+                    if (Input.GetMouseButton(0)){
+                        Vector2 position = results.transform.position;
+                        position = position - (position - results.point);
+                        results.transform.position = new Vector3(position.x, position.y, results.transform.position.z);
+                    }
+                }
+            }
+        } else {
+            SetDefault();
+        }
+        lastMousePos = worldPos;
+	}
+
+    public void SetDefault(){
+        Cursor.SetCursor(mainTex, new Vector2(9, 1), CursorMode.Auto);
+    }
+
+    public void SetGrab()
+    {
+        Cursor.SetCursor(grabTex, new Vector2(9, 1), CursorMode.Auto);
+    }
+}
---
+++ Assets/Scripts/LimitAxis.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Lets you freeze a gameobject's movement in a certain axis!
+public class LimitAxis : MonoBehaviour {
+    [Tooltip("Left, Top, Right, Bottom. -1 means no limit.")]
+    public Vector4 limits = new Vector4(-1, -1, -1, -1);
+    public bool worldSpace = true; //local space not yet supported :(
+
+    private Vector3 startCoords;
+
+	// Use this for initialization
+	void Start () {
+        startCoords = transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 newPos = transform.position;
+        float lLim = limits.x == -1 ? newPos.x : startCoords.x - limits.x;
+        float rLim = limits.z == -1 ? newPos.x : startCoords.x + limits.z;
+        float tLim = limits.y == -1 ? newPos.y : startCoords.y + limits.y;
+        float bLim = limits.w == -1 ? newPos.y : startCoords.y - limits.w;
+
+        newPos.y = Mathf.Clamp(newPos.y, bLim, tLim);
+        newPos.x = Mathf.Clamp(newPos.x, lLim, rLim);
+
+        transform.position = newPos;
+    }
+}
